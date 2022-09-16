bf89ba727da2d8006b9134c401cfe902099cd3d4
DanilStan <104375238+DanilStan@users.noreply.github.com>
2022-09-16 17:50:07 +0000

    Deploying to gh-pages from @ DanilStan/new-breath-band@0ce3f7ba99cf8e4ec0e3e0463a8113a539eb7019 🚀

---
+++ index.7b21d4e0.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".menu");document.querySelectorAll(".menu__item");document.querySelector(".button-open").addEventListener("click",(function(){e.classList.contains("showMenu")?e.classList.remove("showMenu"):e.classList.add("showMenu")}));
+//# sourceMappingURL=index.7b21d4e0.js.map
---
+++ index.98622e52.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".menu");document.querySelectorAll(".menu__item");document.querySelector(".button-open").addEventListener("click",(function(){e.classList.contains("showMenu")?e.classList.remove("showMenu"):e.classList.add("showMenu")}))}();
+//# sourceMappingURL=index.98622e52.js.map
